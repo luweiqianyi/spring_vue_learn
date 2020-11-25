cef22ba154195605f7451030b65967cef8ce664b
YinChao <runningriven@163.com>
2020-11-25 14:11:44 +0800

    Update HelloController.java
    控制器对应的URL访问需要指定端口号输入浏览器才能正确访问到目标地址

--- src/main/java/com/example/chapter01/controller/HelloController.java
+++ src/main/java/com/example/chapter01/controller/HelloController.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    // 谷歌浏览器对于http支持的默认端口号为80端口,若在application.properties中没有指定,
-    // 则不能以http://localhost/hello(注:80端口访问)进行访问，必须以http://localhost:8080/hello进行访问
+    // 在浏览器中访问时需要指定端口号进行访问,端口号在application.properties文件中进行配置
     @GetMapping("/hello")
     public String hello(){
         return "hello spring boot";
