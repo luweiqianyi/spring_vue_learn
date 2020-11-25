6795dc2d3df4d6a0d6b80983ed1c25953ccb3eef
luweiqianyi <runningriven@163.com>
2020-11-25 14:46:58 +0800

    获取配置信息失败： application.properties中自定义配置但是服务器返回 Book{name='null', author='null', price=null}

---
+++ src/main/java/com/example/chapter01/entity/Book.java
@@ -0,0 +1,22 @@
+package com.example.chapter01.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix="book")
+public class Book {
+    private String name;
+    private String author;
+    private Float price;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
